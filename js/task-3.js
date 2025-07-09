82f0ac60e5a836c6a90e44cbf2b9fd3ef92eabca
Denys Orlyk <idsorlyk@gmail.com>
2025-07-09 18:36:10 +0200

    Fix getElementWidth function to use parseFloat for consistent calculations

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 'use strict'
 function getElementWidth(content, padding, border) {
-    return Number.parseInt(content) + 2*Number.parseInt(padding) + 2*Number.parseFloat(border);
+    return Number.parseFloat(content) + 2*Number.parseFloat(padding) + 2*Number.parseFloat(border);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
